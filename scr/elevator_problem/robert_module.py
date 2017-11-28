95720c1f814431afe67cf8e12ad26ce3d8916d8e
R <R@DESKTOP-5DUICBU>
2017-11-28 23:12:08 +0100

    gets retarded

--- scr/elevator_problem/robert_module.py
+++ scr/elevator_problem/robert_module.py
@@ -7,7 +7,16 @@ import elevator_problem.main_controller as main_controller
 from elevator_problem.elevator import Elevator
 import random
     
-
+def better_floors(a_list):
+    new_list = []
+    for entry in a_list:
+        if (entry[1] == "K"):    
+            entry[1].replace("K","-1")
+        elif (entry[1] == "E"):
+            entry[1].replace("E","0")
+        new_list.append(entry)
+    print("new_list 2", new_list)
+            
 def job_sharer(my_list):
     """ Job Sharer.
     
@@ -30,6 +39,8 @@ def job_sharer(my_list):
     print("list b:", joblist_b)
     print("list spec:", job_special)
     
+    joblist_a = better_floors(joblist_a)
+    print(joblist_a)
     return (joblist_a, joblist_b , job_special)
 
 def dispatcher(my_list, tic, elevator_a, elevator_b):
@@ -39,10 +50,21 @@ def dispatcher(my_list, tic, elevator_a, elevator_b):
     tics_to_wait = set[0]
     
     joblist_a, joblist_b, job_special = job_sharer(my_list)
-    curr_pos_a = elevator_a.level
-    curr_pos_b = elevator_b.level
+    curr_pos_a = elevator_a.get_level()
+    curr_pos_b = elevator_b.get_level()
     
-    
+    while(True):
+        curr_pos_a = elevator_a.get_level()
+        curr_pos_b = elevator_b.get_level()
+        inp = input()
+        print("here")
+        for entry in joblist_a:
+            if (inp != False):
+                if (curr_pos_a < int(entry[1])):
+                    elevator_a.set_level(curr_pos_a + 1)
+                    print("A", elevator_a.elevator_printer())
+                    curr_pos_a = elevator_a.get_level()
+                    break
 def main():
     tic = 0
     #  my_list = main_controller.input_reader()
