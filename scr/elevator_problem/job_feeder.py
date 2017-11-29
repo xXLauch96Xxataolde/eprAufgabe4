e20d65d1e14576904e72e632bd7eebe0508ea69a
Niels Heissel <niels.heissel@konzepthaus-ws.de>
2017-11-29 08:43:37 +0100

    logic grundlage

--- scr/elevator_problem/job_feeder.py
+++ scr/elevator_problem/job_feeder.py
@@ -21,15 +21,76 @@ def job_builder(elevator, destination):
     elif elevator.level < destination:
         for lev in range(elevator.level, destination + 1):
             list.append(lev)
-
+    return list
 
 
 def spec_job_assigner(spec_list, job):
 '''assigns the converted jobs to specific job list'''
+
+    if spec_list[takt] == 10:
+        spec_list.extend(job)
+
+    takt_plus_x = 0;
+
     for lev in job:
-        for spec_lev in spec_list[takt:]:  # [takt:] bedeutet schaue dir alle levels nach dem aktuellen takt an
+        for spec_lev in spec_list[takt + takt_plus_x:]:  # [takt:] bedeutet schaue dir alle levels nach dem aktuellen takt an
+
+
+            job_parser
+
+
             if lev == spec_lev:
+
                 print("match")
+                treffer = True
+                takt_plus_x += 1
+                continue
+
+            elif treffer is True:
+                print("no match anymore check new element")
+
                 break
 
+            else: print("first new element")
 
+            takt_plus_x += 1
+
+        if takt_plus_x == len(spec_list[takt:]) and treffer is True:
+            print("append at last char")
+
+    if treffer is True:
+        spec_list.extend(job[takt_plus_x - 1:])
+
+direction = ""
+match = False
+new_takt = -1
+def allgemeiner_job_assigner(job_liste, elevator, spec_list):
+    for job in job_liste:
+
+        for lev in spec_list[takt:]:
+            new_takt += 1
+            if job[0] == lev:
+                print("elevator passes level")
+                match = True
+
+            if match is True:
+                if spec_list[takt + new_takt] > spec_list[takt + new_takt + 1]:
+                    print("fährt runter")
+                    direction = "r"
+                elif spec_list[takt + new_takt] < spec_list[takt + new_takt + 1]:
+                    print("fährt hoch")
+                    direction = "h"
+
+                if direction == job[1] and match is True:
+                    print("WE HAVE A MATCH")
+
+                else: continue  #or evtl. break
+
+
+
+2r
+3h
+
+1234
+
+1
