c70d716702b821b6631a3a4fcf3d31c2ef89953b
R <R@DESKTOP-5DUICBU>
2017-12-02 20:49:16 +0100

    moved the gui levitation added pictures

--- scr/elevator_problem/gui_levitation.py
+++ scr/elevator_problem/gui_levitation.py
@@ -1,70 +1,39 @@
-"""The Test Function
+"""Gui Levitation
 
-This function is for testing purposes 
-important video: https://www.youtube.com/watch?v=h3Yrhv33Zb8
+This function provides the wanted visualization of the elevators. 
+A little poorly executed but maybe we get there
 """
-
+from tkinter import *
+from tkinter.ttk import *
 import time
 
-__author__ = "123456: Ada Lovelace, 654321: Alan Turing"
-__copyright__ = "Copyright 2017/2018 - EPR-Goethe-Uni" 
-__credits__ = "" 
-__email__ = "uni.goethe.horde@gmail.com" 
-
-import tkinter as Tkinter
-import tkinter.ttk as ttk
-import time
-
-
-def ele_move(step, elevator_bar):
-    for i in range(20):
-        time.sleep(0.1)
-        elevator_bar.step(step)
-        elevator_bar.update()
+tk = Tk()
+image_left = PhotoImage(file='abc.gif')
+image_right = PhotoImage(file='cba.gif')
+label_house = Label(tk, image=image_left)
+label_house.pack(side="left", fill="x", expand="True")
+label_house2 = Label(tk, image=image_right)
+label_house2.pack(side="right", fill="x", expand="True")
+elevator_a = Progressbar(tk, orient=VERTICAL, length=100, mode='indeterminate')
+elevator_a.pack(side="left", fill="x", expand=False, padx=8, pady=4)
+elevator_b = Progressbar(tk, orient=VERTICAL, length=100, mode='indeterminate')
+elevator_b.pack(side="left", fill='x', expand=False, padx=8, pady=4)
+# help
 
 
-def lvl(level):
-    dict = {-1: "Basement",
-            0: "Ground Floor",
-            1 : "1",
-            2 : "2",
-            3 : "3",
-            4 : "4",
-            }
-    print("Elevator Level:", dict.get(level))
-
-
-def test1():
-    level = 0
-    root = Tkinter.Tk()
+def bar():
+    for i in range(5):
+        time.sleep(1)
+        elevator_a_label.configure(text="Elevator A:" + str(i))
+        tk.update_idletasks()
     
-    ft = ttk.Frame()
-    fb = ttk.Frame()
-    
-    ft.pack(expand=True, fill=Tkinter.BOTH, side=Tkinter.TOP)
-    fb.pack(expand=True, fill=Tkinter.BOTH, side=Tkinter.TOP)
-    
-    elevator_bar = ttk.Progressbar(fb, orient='vertical', mode='indeterminate', value=100)
-   
-    elevator_bar.pack(expand=True, fill=Tkinter.BOTH, side=Tkinter.LEFT)
+    tk.update_idletasks()
+    elevator_a['value'] = 20  
+    time.sleep(1) 
 
-    elevator_bar.update()
-    
-    while(True):
-        # elevator_bar.step(0)
-        # elevator_bar.update()
-        inp = input()
-        if(inp == "up"):
-            ele_move(1, elevator_bar)
-            level += 1
-            lvl(level)
-        elif(inp == "down"):
-            ele_move(-1, elevator_bar)
-            level -= 1
-            lvl(level)
+elevator_a_label = Label(tk, text="yddas")
+elevator_a_label.pack()
 
-    root.mainloop()
+Button(tk, text="Start", command=bar).pack(side="bottom", fill='both', expand=False, padx=4, pady=2)
 
-
-if __name__ == '__main__':
-    main()
+mainloop()
