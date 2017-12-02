1034031269d2e950a397064689f4a8b79ccf6057
R <R@DESKTOP-5DUICBU>
2017-12-02 15:10:48 +0100

    built a menue

--- scr/elevator_problem/main.py
+++ scr/elevator_problem/main.py
@@ -12,11 +12,32 @@ __copyright__ = "Copyright 2017/2018 - EPR-Goethe-Uni"
 __credits__ = ""
 __email__ = "uni.goethe.horde@gmail.com"
 
+def menue():
+    
+    print("...Welcome to the Menue................")
+    print(".......................................")
+    print("...Press 1 to open standard Elevator...")
+    print("...Press 2 to open improved Elevator...")
+    print("...Press 3 for Help File...............")
+    print("...Press 4 to Exit.....................")
+    print(".......................................")
 
 def main():
     # gui_levitation.test()
-    main_controller.main_function()  # maybe rename these modules
-
+    
+    while (True):
+        menue()
+        inp = input()
+        if (inp == "1"):
+            main_controller.main_function()  # maybe rename these modules
+        elif (inp == "2"):
+            print("Same procedure but imporved")
+        elif (inp == "3"):
+            print("Help File")
+        elif (inp == "4"):
+            break
+        else:
+            print("Invalid Input. Please reoeat")
 
 if __name__ == '__main__':
     main()
