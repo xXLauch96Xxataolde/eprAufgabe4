4e8214f4794d4d3c78917ef8e7b297d8973b5903
R <R@DESKTOP-5DUICBU>
2017-12-04 21:46:29 +0100

    commenting on main_controller

---
+++ scr/elevator_problem/docu_handling.py
@@ -0,0 +1,26 @@
+"""An Elevator Class
+
+If the constructor of this class is called, an elevator object is constructed.
+We gave them quite a few attributes and functions as well a pocedures. That
+helped us in the development. 
+
+"""
+
+import elevator_problem
+import main
+import main_controller
+import elevator
+import job_feeder
+
+__author__ = "6770541: Niels Heissel, 6785468: Robert am Wege"
+__copyright__ = "Copyright 2017/2018 - EPR-Goethe-Uni" 
+__credits__ = "" 
+__email__ = "uni.goethe.horde@gmail.com" 
+
+"""print all help file for documentation purposes"""
+
+print(help(elevator_problem))
+print(help(main))
+print(help(main_controller))
+print(help(elevator))
+print(help(job_feeder))
