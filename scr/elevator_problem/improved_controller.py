3d2d99028049737d7e8cad7af6305f11521b38cf
Niels Heissel <niels.heissel@konzepthaus-ws.de>
2017-12-05 13:01:40 +0100

    Second Improvement done!

--- scr/elevator_problem/improved_controller.py
+++ scr/elevator_problem/improved_controller.py
@@ -1,6 +1,6 @@
 """The Controller Function
 
-This module is meant for coordination purposes of the program, which is responsible for the normal elevator.
+This module is meant for coordination purposes of the program, which is responsible for the improved elevator.
 """
 
 import time
