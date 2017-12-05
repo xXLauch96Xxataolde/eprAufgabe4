0592407804a50569170cc1f7998a15acd5dbf3b5
Robert <Robert@172.22.147.181>
2017-12-05 15:27:45 +0100

    lalala

---
+++ scr/elevator_problem/__init__.py
@@ -0,0 +1,6 @@
+"""Just the default package init for the interpreter"""
+
+__author__ = "6770541: Niels Heissel, 6785468: Robert am Wege"
+__copyright__ = "Copyright 2017/2018 - EPR-Goethe-Uni" 
+__credits__ = "" 
+__email__ = "uni.goethe.horde@gmail.com" 
